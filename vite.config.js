a3ab0bf7a1373fb4b75642317ab2b74008560716
MyroslavShendrik <citrus.user2204@gmail.com>
2025-09-19 15:33:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   base: '/webstudio-scss-vite/',
-  root: 'src', // корінь
+  root: 'src', // src вже root
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: glob.sync('**/*.html', { ignore: ['node_modules/**'] }).reduce((entries, file) => {
-        // не додаємо 'src' два рази
-        entries[file] = path.resolve(__dirname, 'src', file);
+      input: glob.sync('*.html', { ignore: ['node_modules/**'] }).reduce((entries, file) => {
+        // Тільки ім'я файлу або шлях відносно root
+        entries[file] = path.resolve(__dirname, 'src', file); // тут file відносно src
         return entries;
       }, {}),
     },
