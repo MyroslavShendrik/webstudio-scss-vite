dd66e6c900b5f29a6016e718609dbf372288adb2
MyroslavShendrik <citrus.user2204@gmail.com>
2025-09-19 15:29:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite';
 import glob from 'glob';
+import path from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import path from 'path';
 
 export default defineConfig({
   base: '/webstudio-scss-vite/',
   root: 'src',
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: glob.sync('**/*.html', { ignore: ['node_modules/**'] }).reduce((entries, file) => {
         entries[file] = path.resolve(__dirname, 'src', file);
         return entries;
       }, {}),
     },
-    outDir: '../dist',
-    emptyOutDir: true,
   },
   plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
 });
