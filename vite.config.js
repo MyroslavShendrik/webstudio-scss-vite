fb233bc83a4ffcfa311abd24e01209e38af2bc04
MyroslavShendrik <citrus.user2204@gmail.com>
2025-09-21 08:41:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: glob.sync('./src/**/*.html').reduce((entries, file) => {
-        const name = path.relative('src', file); // прибираємо "src/"
+        const name = path.parse(file).name; // тільки ім’я без шляху
         entries[name] = file;
         return entries;
       }, {}),
