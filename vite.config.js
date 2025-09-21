e819011a0b4f1b21be97090b028e2f1a2ed0d463
MyroslavShendrik <citrus.user2204@gmail.com>
2025-09-21 08:59:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,16 @@ import path from 'path';
 
 export default defineConfig({
   base: '/webstudio-scss-vite/',
-  root: 'src',
+  root: path.resolve(__dirname, 'src'), // повний шлях root
   build: {
-    outDir: '../dist',
+    outDir: path.resolve(__dirname, 'dist'), // повний шлях для збірки
     rollupOptions: {
-      input: glob.sync('**/*.html', { cwd: 'src' }).reduce((entries, file) => {
-        // file відносно root
-        entries[file] = path.resolve(__dirname, 'src', file);
+      input: glob.sync('**/*.html', { cwd: path.resolve(__dirname, 'src'), absolute: true }).reduce((entries, file) => {
+        const name = path.relative(path.resolve(__dirname, 'src'), file);
+        entries[name] = file;
         return entries;
       }, {}),
     },
   },
-  plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+  plugins: [injectHTML(), FullReload([path.resolve(__dirname, 'src/**/*.html')])],
 });
-
